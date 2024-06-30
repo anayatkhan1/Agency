7b7dbf3221df8d4f6ef5e7675315f96005227a25
Anayat Khan <anayat0khan@gmail.com>
2024-06-30 14:46:19 +0530

    work logo change

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -18,38 +18,40 @@ import logoPhobiaDark from '@/images/clients/phobia/logo-dark.svg'
 import logoPhobiaLight from '@/images/clients/phobia/logo-light.svg'
 import logoUnseal from '@/images/clients/unseal/logo-light.svg'
 import imageLaptop from '@/images/laptop.jpg'
+import { aubree, core, oneheadlight, swiggy, vardaan, juniper, myshell } from '@/images/work'
 import { loadMDXMetadata } from '@/lib/loadMDXMetadata'
 
 const clients = [
-  ['Phobia', logoPhobiaLight],
-  ['Family Fund', logoFamilyFund],
-  ['Unseal', logoUnseal],
-  ['Mail Smirk', logoMailSmirk],
-  ['Home Work', logoHomeWork],
-  ['Green Life', logoGreenLife],
-  ['Bright Path', logoBrightPath],
-  ['North Adventures', logoNorthAdventures],
+  ['Vardaan', vardaan],
+  ['Success Core', core],
+  ['Myshell', myshell],
+  ['Swiggy', swiggy],
+  ['Juniper', juniper],
+  ['Aubree', aubree],
+  ['OneHeadlight', oneheadlight],
+ 
+  // ['North Adventures', logoNorthAdventures],
 ]
 
 function Clients() {
   return (
-    <div className="mt-24 rounded-4xl bg-neutral-950 py-20 sm:mt-32 sm:py-32 lg:mt-56">
+    <div className="py-20 mt-24 rounded-4xl bg-neutral-950 sm:mt-32 sm:py-32 lg:mt-56">
       <Container>
         <FadeIn className="flex items-center gap-x-8">
-          <h2 className="text-center font-display text-sm font-semibold tracking-wider text-white sm:text-left">
+          <h2 className="text-sm font-semibold tracking-wider text-center text-white font-display sm:text-left">
             We’ve worked with hundreds of amazing people
           </h2>
-          <div className="h-px flex-auto bg-neutral-800" />
+          <div className="flex-auto h-px bg-neutral-800" />
         </FadeIn>
         <FadeInStagger faster>
           <ul
             role="list"
-            className="mt-10 grid grid-cols-2 gap-x-8 gap-y-10 lg:grid-cols-4"
+            className="grid grid-cols-2 mt-10 gap-x-8 gap-y-10 lg:grid-cols-4"
           >
             {clients.map(([client, logo]) => (
               <li key={client}>
-                <FadeIn>
-                  <Image src={logo} alt={client} unoptimized />
+                <FadeIn >
+                <Image className="" src={logo} alt={client} />
                 </FadeIn>
               </li>
             ))}
@@ -77,19 +79,19 @@ function CaseStudies({ caseStudies }) {
         <FadeInStagger className="grid grid-cols-1 gap-8 lg:grid-cols-3">
           {caseStudies.map((caseStudy) => (
             <FadeIn key={caseStudy.href} className="flex">
-              <article className="relative flex w-full flex-col rounded-3xl p-6 ring-1 ring-neutral-950/5 transition hover:bg-neutral-50 sm:p-8">
+              <article className="relative flex flex-col w-full p-6 transition rounded-3xl ring-1 ring-neutral-950/5 hover:bg-neutral-50 sm:p-8">
                 <h3>
                   <Link href={caseStudy.href}>
                     <span className="absolute inset-0 rounded-3xl" />
                     <Image
                       src={caseStudy.logo}
                       alt={caseStudy.client}
-                      className="h-16 w-16"
+                      className="w-16 h-16"
                       unoptimized
                     />
                   </Link>
                 </h3>
-                <p className="mt-6 flex gap-x-2 text-sm text-neutral-950">
+                <p className="flex mt-6 text-sm gap-x-2 text-neutral-950">
                   <time dateTime={caseStudy.year} className="font-semibold">
                     {caseStudy.date.split('-')[0]}
                   </time>
@@ -98,7 +100,7 @@ function CaseStudies({ caseStudies }) {
                   </span>
                   <span>Case study</span>
                 </p>
-                <p className="mt-6 font-display text-2xl font-semibold text-neutral-950">
+                <p className="mt-6 text-2xl font-semibold font-display text-neutral-950">
                   {caseStudy.title}
                 </p>
                 <p className="mt-4 text-base text-neutral-600">
@@ -177,16 +179,14 @@ export default async function Home() {
       <Container className="mt-24 sm:mt-32 md:mt-56">
         <FadeIn className="max-w-3xl">
           <h1 className="font-display text-5xl font-medium tracking-tight text-neutral-950 [text-wrap:balance] sm:text-7xl">
-            Award-winning development studio based in Arkansas.
+          Development Studio Bridging Design and Technology.
           </h1>
           <p className="mt-6 text-xl text-neutral-600">
-            We are a development studio working at the intersection of design
-            and technology. It’s a really busy intersection though — a lot of
-            our staff have been involved in hit and runs.
+          We&apos;re a development studio thriving at the crossroads of design and technology. 
+          It&apos;s a bustling intersection — our team often finds themselves in the midst of exciting challenges and creative collisions.
           </p>
         </FadeIn>
       </Container>
-
       <Clients />
 
       <CaseStudies caseStudies={caseStudies} />
---
+++ src/images/work/index.js
@@ -0,0 +1,16 @@
+import aubree from "@/images/work/aubree.svg"
+import core from "@/images/work/core.svg"
+import oneheadlight from "@/images/work/oneheadlight.svg"
+import swiggy from "@/images/work/swiggy.svg"
+import vardaan from '@/images/work/vardaan.svg'
+import juniper from "@/images/work/juniper.svg"
+import myshell from '@/images/work/myshell.svg'
+export {
+    aubree,
+    core,
+    oneheadlight,
+    swiggy, 
+    vardaan,
+    juniper,
+    myshell
+}
